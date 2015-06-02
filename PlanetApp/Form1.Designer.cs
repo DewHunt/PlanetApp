e940f915bac92ed6b8e050b7e1df9a6e0a164126
s-sabbir <dew.fog1553@gmail.com>
2015-06-03 01:18:05 +0600

    Initial Commit for PlanetApp

---
+++ PlanetApp/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PlanetApp
+{
+    partial class planetPositionUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.planetNameLebel = new System.Windows.Forms.Label();
+            this.showPlanetButton = new System.Windows.Forms.Button();
+            this.planetNameComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // planetNameLebel
+            // 
+            this.planetNameLebel.AutoSize = true;
+            this.planetNameLebel.Location = new System.Drawing.Point(142, 47);
+            this.planetNameLebel.Name = "planetNameLebel";
+            this.planetNameLebel.Size = new System.Drawing.Size(68, 13);
+            this.planetNameLebel.TabIndex = 1;
+            this.planetNameLebel.Text = "Planet Name";
+            // 
+            // showPlanetButton
+            // 
+            this.showPlanetButton.Location = new System.Drawing.Point(249, 80);
+            this.showPlanetButton.Name = "showPlanetButton";
+            this.showPlanetButton.Size = new System.Drawing.Size(160, 21);
+            this.showPlanetButton.TabIndex = 2;
+            this.showPlanetButton.Text = "Show Planet";
+            this.showPlanetButton.UseVisualStyleBackColor = true;
+            this.showPlanetButton.Click += new System.EventHandler(this.showPlanetButton_Click);
+            // 
+            // planetNameComboBox
+            // 
+            this.planetNameComboBox.FormattingEnabled = true;
+            this.planetNameComboBox.Location = new System.Drawing.Point(249, 44);
+            this.planetNameComboBox.Name = "planetNameComboBox";
+            this.planetNameComboBox.Size = new System.Drawing.Size(160, 21);
+            this.planetNameComboBox.TabIndex = 3;
+            // 
+            // planetPositionUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 317);
+            this.Controls.Add(this.planetNameComboBox);
+            this.Controls.Add(this.showPlanetButton);
+            this.Controls.Add(this.planetNameLebel);
+            this.Name = "planetPositionUI";
+            this.Text = "Planet Position";
+            this.Load += new System.EventHandler(this.planetPositionUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label planetNameLebel;
+        private System.Windows.Forms.Button showPlanetButton;
+        private System.Windows.Forms.ComboBox planetNameComboBox;
+    }
+}
+
---
+++ PlanetApp/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlanetApp
+{
+    public partial class planetPositionUI : Form
+    {
+        public planetPositionUI()
+        {
+            InitializeComponent();
+        }
+
+        private void showPlanetButton_Click(object sender, EventArgs e)
+        {
+            if(planetNameComboBox.Text == "")
+            {
+                MessageBox.Show("Please enter planet name");
+            }
+            else
+            {
+                string position = "";
+                string planetName = planetNameComboBox.Text;
+
+                if (planetName == "Mercury")
+                {
+                    position = planetName + " is in the 3rd Position";
+                }
+
+                if (planetName == "Venus")
+                {
+                    position = planetName + " is in the 3rd Position";
+                }
+
+                if (planetName == "Earth")
+                {
+                    position = planetName + " is in the 3rd Position";
+                }
+
+                if (planetName == "Mars")
+                {
+                    position = planetName + " is in the 3rd Position";
+                }
+
+                if (planetName == "Jupiter")
+                {
+                    position = planetName + " is in the 3rd Position";
+
+                    //MessageBox.Show("It is in the 3rd Position");
+                }
+
+                if (planetName == "Saturn")
+                {
+                    position = planetName + " is in the 6th Position";
+                }
+
+                if (planetName == "Uranus")
+                {
+                    position = planetName + " is in the 7th Position";
+                }
+
+                if (planetName == "Neptune")
+                {
+                    position = planetName + " is in the 8th Position";
+                }
+
+                if (planetName == "Pluto")
+                {
+                    position = planetName + " is in the 9th Position";
+                }
+
+                MessageBox.Show(position);
+            }
+        }
+
+        private void planetPositionUI_Load(object sender, EventArgs e)
+        {
+            planetNameComboBox.Items.Add("Mercury");
+            planetNameComboBox.Items.Add("Venus");
+            planetNameComboBox.Items.Add("Earth");
+            planetNameComboBox.Items.Add("Mars");
+            planetNameComboBox.Items.Add("Jupiter");
+            planetNameComboBox.Items.Add("Saturn");
+            planetNameComboBox.Items.Add("Uranus");
+            planetNameComboBox.Items.Add("Neptune");
+            planetNameComboBox.Items.Add("Pluto");
+            planetNameComboBox.SelectedItem = "Earth";
+        }
+    }
+}
